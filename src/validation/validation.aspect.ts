fa536023e71d69fc1243b2805d5449c622f5084e
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2019-06-21 10:51:36 +0700

    Make compound validator reusable

--- src/validation/validation.aspect.ts
+++ src/validation/validation.aspect.ts
@@ -1,18 +1,8 @@
-import { NextArgs, nextArgs, noop } from 'call-thru';
-import {
-  AfterEvent,
-  AfterEvent__symbol,
-  afterEventBy,
-  afterEventFrom,
-  EventEmitter,
-  eventInterest,
-  EventInterest,
-  EventKeeper,
-  isEventKeeper,
-} from 'fun-events';
+import { AfterEvent, AfterEvent__symbol, afterEventFrom, EventInterest, EventKeeper, } from 'fun-events';
 import { InAspect, InAspect__symbol } from '../aspect';
 import { InControl } from '../control';
 import { InValidator } from './validator';
+import { InValidationMessages } from './validator.impl';
 
 const InValidation__aspect: Aspect = {
   applyTo<Value>(control: InControl<Value>): Applied<Value> {
@@ -161,118 +151,6 @@ export namespace InValidation {
 
   }
 
-}
-
-class InValidationMessages<Value> implements EventKeeper<InValidation.Message[]> {
-
-  readonly [AfterEvent__symbol]: AfterEvent<InValidation.Message[]>;
-  readonly from: (this: void, validator: InValidator<Value>) => EventInterest;
-
-  constructor(control: InControl<Value>) {
-
-    const emitter = new EventEmitter<InValidation.Message[]>();
-    const validators = new Map<AfterEvent<InValidation.Message[]>, EventInterest>();
-    const validatorMessages = new Map<InValidator<Value>, InValidation.Message[]>();
-    // Sends validation messages
-    let send: () => void = noop;
-    // Validates using the given validator
-    let validate: (validator: AfterEvent<InValidation.Message[]>, validatorInterest: EventInterest) => void = noop;
-
-    this[AfterEvent__symbol] = afterEventBy(receiver => {
-
-      // An interest for receiving validation messages
-      const resultInterest = afterEventFrom(emitter, [])(receiver).whenDone(() => {
-        send = noop; // Disable message sending
-        validate = noop; // Disable validation
-      });
-
-      // Enable validation using the given validator
-      validate = (validator: AfterEvent<InValidation.Message[]>, validatorInterest: EventInterest) => {
-        validator((...messages) => {
-          if (messages.length) {
-            // Replace messages reported by validator.
-            validatorMessages.set(validator, messages);
-          } else if (!validatorMessages.delete(validator)) {
-            // Nothing removed. No need to send messages
-            return;
-          }
-          send(); // Send all messages.
-        }).needs(validatorInterest).needs(resultInterest);
-      };
-
-      // Enable each validator
-      for (const [validator, validatorInterest] of validators.entries()) {
-        validate(validator, validatorInterest);
-      }
-
-      // Enable message sending
-      send = () => {
-        emitter.send(...allMessages());
-      };
-
-      // Send messages if present already
-      if (validatorMessages.size) {
-        send();
-      }
-
-      return resultInterest;
-    }).share();
-
-    this.from = validator => {
-
-      const source = controlValidator(control, validator);
-      const validatorInterest = eventInterest(() => {
-        // Remove the validator
-        validators.delete(source);
-        if (validatorMessages.delete(source)) {
-          // Send all messages only if the removed validator reported some messages earlier
-          send();
-        }
-      });
-
-      validators.set(source, validatorInterest);
-      validate(source, validatorInterest); // Start validation using validator
-
-      return validatorInterest;
-    };
-
-    function *allMessages(): Iterable<InValidation.Message> {
-      for (const messages of validatorMessages.values()) {
-        yield *messages;
-      }
-    }
-  }
-
-}
-
-function controlValidator<Value>(
-    control: InControl<Value>,
-    validator: InValidator<Value>
-): AfterEvent<InValidation.Message[]> {
-  if (isEventKeeper(validator)) {
-    return afterEventFrom(validator);
-  }
-  if (typeof validator === 'function') {
-    return afterEventFrom(validator(control));
-  }
-
-  return control.read.keep.thru(simpleValidator(control, validator));
-}
-
-function simpleValidator<Value>(
-    control: InControl<Value>,
-    validator: InValidator.Simple<Value>,
-): <NextReturn>(value: Value) => NextArgs<InValidation.Message[], NextReturn> | InValidation.Message {
-  return <NextReturn>() => {
-
-    const messages = validator.validate(control);
-
-    return messages == null
-        ? nextArgs()
-        : Array.isArray(messages)
-            ? nextArgs(...messages)
-            : messages;
-  };
 }
 
 const noValidationErrors: InValidation.Result = {
---
+++ src/validation/validator.impl.ts
@@ -0,0 +1,134 @@
+import { nextArgs, NextArgs, noop } from 'call-thru';
+import {
+  AfterEvent,
+  AfterEvent__symbol,
+  afterEventBy,
+  afterEventFrom,
+  EventEmitter,
+  eventInterest,
+  EventInterest,
+  EventKeeper,
+  isEventKeeper
+} from 'fun-events';
+import { InControl } from '../control';
+import { InValidation } from './validation.aspect';
+import { InValidator } from './validator';
+
+/**
+ * @internal
+ */
+export class InValidationMessages<Value> implements EventKeeper<InValidation.Message[]> {
+
+  readonly [AfterEvent__symbol]: AfterEvent<InValidation.Message[]>;
+  readonly from: (this: void, validator: InValidator<Value>) => EventInterest;
+
+  constructor(control: InControl<Value>) {
+
+    const emitter = new EventEmitter<InValidation.Message[]>();
+    const validators = new Map<AfterEvent<InValidation.Message[]>, EventInterest>();
+    const validatorMessages = new Map<InValidator<Value>, InValidation.Message[]>();
+    // Sends validation messages
+    let send: () => void = noop;
+    // Validates using the given validator
+    let validate: (validator: AfterEvent<InValidation.Message[]>, validatorInterest: EventInterest) => void = noop;
+
+    this[AfterEvent__symbol] = afterEventBy(receiver => {
+
+      // An interest for receiving validation messages
+      const resultInterest = afterEventFrom(emitter, [])(receiver).whenDone(() => {
+        send = noop; // Disable message sending
+        validate = noop; // Disable validation
+      });
+
+      // Enable validation using the given validator
+      validate = (validator: AfterEvent<InValidation.Message[]>, validatorInterest: EventInterest) => {
+        validator((...messages) => {
+          if (messages.length) {
+            // Replace messages reported by validator.
+            validatorMessages.set(validator, messages);
+          } else if (!validatorMessages.delete(validator)) {
+            // Nothing removed. No need to send messages
+            return;
+          }
+          send(); // Send all messages.
+        })
+            .needs(resultInterest)
+            .needs(validatorInterest)
+            .whenDone(() => {
+              if (validatorMessages.delete(validator)) {
+                // Send all messages only if the removed validator reported some messages earlier
+                send();
+              }
+            });
+      };
+
+      // Enable each validator
+      for (const [validator, validatorInterest] of validators.entries()) {
+        validate(validator, validatorInterest);
+      }
+
+      // Enable message sending
+      send = () => {
+        emitter.send(...allMessages());
+      };
+
+      // Send messages if present already
+      if (validatorMessages.size) {
+        send();
+      }
+
+      return resultInterest;
+    }).share();
+
+    this.from = validator => {
+
+      const source = controlValidator(control, validator);
+      const validatorInterest = eventInterest(() => {
+        validators.delete(source);
+      });
+
+      validators.set(source, validatorInterest);
+      validate(source, validatorInterest); // Start validation using validator
+
+      return validatorInterest;
+    };
+
+    function *allMessages(): Iterable<InValidation.Message> {
+      for (const messages of validatorMessages.values()) {
+        yield *messages;
+      }
+    }
+
+  }
+
+}
+
+function controlValidator<Value>(
+    control: InControl<Value>,
+    validator: InValidator<Value>
+): AfterEvent<InValidation.Message[]> {
+  if (isEventKeeper(validator)) {
+    return afterEventFrom(validator);
+  }
+  if (typeof validator === 'function') {
+    return afterEventFrom(validator(control));
+  }
+
+  return control.read.keep.thru(simpleValidator(control, validator));
+}
+
+function simpleValidator<Value>(
+    control: InControl<Value>,
+    validator: InValidator.Simple<Value>,
+): <NextReturn>(value: Value) => NextArgs<InValidation.Message[], NextReturn> | InValidation.Message {
+  return <NextReturn>() => {
+
+    const messages = validator.validate(control);
+
+    return messages == null
+        ? nextArgs()
+        : Array.isArray(messages)
+            ? nextArgs(...messages)
+            : messages;
+  };
+}
