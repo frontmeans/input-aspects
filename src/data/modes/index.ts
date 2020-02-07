c620182c168e9c1a4d564cdc12a9c35042e6f03d
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-02-07 17:36:04 +0700

    Add `inModeByValidity()`

--- src/data/modes/index.ts
+++ src/data/modes/index.ts
@@ -1,1 +1,2 @@
 export * from './mode-by-form';
+export * from './mode-by-validity';
---
+++ src/data/modes/mode-by-validity.spec.ts
@@ -0,0 +1,46 @@
+import { afterThe } from 'fun-events';
+import { InControl } from '../../control';
+import { InValidation } from '../../validation';
+import { inValue } from '../../value';
+import { InMode } from '../mode.aspect';
+import { inModeByValidity } from './mode-by-validity';
+
+describe('inModeByValidity', () => {
+
+  let form: InControl<any>;
+  let validation: InValidation<any>;
+  let mode: InMode;
+  let modeValue: InMode.Value;
+
+  beforeEach(() => {
+    form = inValue('form');
+    validation = form.aspect(InValidation);
+    mode = form.aspect(InMode);
+    mode.read(m => modeValue = m);
+  });
+
+  it('is enabled for valid form', () => {
+    mode.derive(inModeByValidity());
+    expect(modeValue).toBe('on');
+  });
+  it('is set to not submittable on validation errors by default', () => {
+    mode.derive(inModeByValidity());
+    validation.by(afterThe({ invalid: true }));
+    expect(modeValue).toBe('-on');
+  });
+  it('is set to custom value on validation errors', () => {
+    mode.derive(inModeByValidity({ invalid: 'off' }));
+    validation.by(afterThe({ invalid: true }));
+    expect(modeValue).toBe('off');
+  });
+  it('is enabled on submit errors by default', () => {
+    mode.derive(inModeByValidity());
+    validation.by(afterThe({ submit: true, invalid: true }));
+    expect(modeValue).toBe('on');
+  });
+  it('is enabled on custom ignored errors', () => {
+    mode.derive(inModeByValidity({ ignore: ['custom'] }));
+    validation.by(afterThe({ custom: true, invalid: true }));
+    expect(modeValue).toBe('on');
+  });
+});
