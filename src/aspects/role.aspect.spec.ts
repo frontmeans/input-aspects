44cdba105ec32aae75353006e0d197f142905cbe
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2021-03-04 17:08:54 +0700

    Add `InRole` aspect

---
+++ src/aspects/role.aspect.spec.ts
@@ -0,0 +1,223 @@
+import { afterSupplied } from '@proc7ts/fun-events';
+import { asis, Supply } from '@proc7ts/primitives';
+import { InControl } from '../control';
+import { InStyledElement } from '../elements/style';
+import { inValue } from '../value.control';
+import { InRole } from './role.aspect';
+
+describe('InRole', () => {
+
+  let control: InControl<string>;
+  let role: InRole<string>;
+
+  beforeEach(() => {
+    control = inValue('test');
+    role = control.aspect(InRole);
+  });
+
+  it('is not convertible', async () => {
+    role.add('test');
+
+    const converted = control.convert(InStyledElement.to(document.createElement('div')));
+    const convertedRole = await converted.aspect(InRole).read;
+
+    expect(convertedRole.has('test')).toBe(false);
+  });
+
+  describe('add', () => {
+    it('enables role', async () => {
+
+      role.add('test');
+
+      const active = await control.aspect(InRole).read;
+
+      expect(active.has('test')).toBe(true);
+      expect([...active]).toEqual(['test']);
+    });
+    it('disables role when all supplies cut off', async () => {
+
+      const supply1 = role.add('test');
+      const supply2 = role.add('test');
+
+      expect((await afterSupplied(role)).has('test')).toBe(true);
+
+      supply2.off();
+      expect((await afterSupplied(role)).has('test')).toBe(true);
+
+      supply1.off();
+      expect((await afterSupplied(role)).has('test')).toBe(false);
+    });
+  });
+
+  describe('when', () => {
+    it('activates already enabled role', async () => {
+
+      const activator = jest.fn(() => new Supply());
+
+      role.add('test');
+
+      role.when('test', activator);
+      expect(activator).toHaveBeenCalledWith(control, 'test', await role.read);
+    });
+    it('activates the role when it is added', async () => {
+
+      const activator = jest.fn(() => new Supply());
+
+      role.when('test', activator);
+      expect(activator).not.toHaveBeenCalled();
+
+      role.add('test');
+      expect(activator).toHaveBeenCalledWith(control, 'test', await role.read);
+    });
+    it('re-activates the role when it is added again', () => {
+
+      const activator = jest.fn(() => new Supply());
+
+      role.when('test', activator);
+
+      role.add('test').off();
+      role.add('test');
+
+      expect(activator).toHaveBeenCalledTimes(2);
+    });
+    it('cuts off activation supply once the role disabled', async () => {
+
+      const activationSupply = new Supply();
+
+      role.when('test', () => activationSupply);
+      role.add('test').off('reason');
+
+      expect(activationSupply.isOff).toBe(true);
+      expect(await activationSupply.whenDone().catch(asis)).toBe('reason');
+    });
+    it('does not call removed activator', () => {
+
+      const activator1 = jest.fn(() => new Supply());
+      const activator2 = jest.fn(() => new Supply());
+
+      const supply1 = role.when('test', activator1);
+      const supply2 = role.when('test', activator2);
+
+      supply1.off();
+      role.add('test').off();
+
+      supply2.off();
+      role.add('test').off();
+
+      expect(activator1).not.toHaveBeenCalled();
+      expect(activator2).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('default role', () => {
+    it('is active by default', async () => {
+
+      const active = await control.aspect(InRole).read;
+
+      expect(active.has('default')).toBe(true);
+      expect([...active]).toEqual(['default']);
+    });
+    it('becomes inactive when other role added', async () => {
+      role.add('test');
+
+      const active = await control.aspect(InRole).read;
+
+      expect(active.has('default')).toBe(false);
+      expect(active.has('test')).toBe(true);
+      expect([...active]).toEqual(['test']);
+    });
+    it('becomes active when all other roles removed', async () => {
+
+      const supply1 = role.add('test');
+      const supply2 = role.add('test');
+      const supply3 = role.add('test3');
+
+      supply1.off();
+      supply2.off();
+      supply3.off();
+
+      const active = await control.aspect(InRole).read;
+
+      expect(active.has('default')).toBe(true);
+      expect(active.has('test')).toBe(false);
+      expect(active.has('test3')).toBe(false);
+      expect([...active]).toEqual(['default']);
+    });
+    it('can be enabled explicitly', async () => {
+
+      role.add('test1');
+      role.add('default');
+      role.add('test2');
+
+      const active = await control.aspect(InRole).read;
+
+      expect(active.has('test1')).toBe(true);
+      expect(active.has('default')).toBe(true);
+      expect(active.has('test2')).toBe(true);
+      expect([...active]).toEqual(['test1', 'default', 'test2']);
+    });
+    it('becomes active when `default` role removed', async () => {
+
+      const supply = role.add('default');
+
+      const active1 = await control.aspect(InRole).read;
+
+      expect(active1.has('default')).toBe(true);
+      expect([...active1]).toEqual(['default']);
+
+      supply.off();
+
+      const active2 = await control.aspect(InRole).read;
+
+      expect(active2.has('default')).toBe(true);
+      expect([...active2]).toEqual(['default']);
+    });
+    it('remains active when explicitly enabled and other roles removed', async () => {
+
+      const supply1 = role.add('default');
+      const supply2 = role.add('test');
+      const supply3 = role.add('default');
+
+      supply1.off();
+      supply2.off();
+
+      const active = await control.aspect(InRole).read;
+
+      expect(active.has('default')).toBe(true);
+      expect(active.has('test')).toBe(false);
+      expect([...active]).toEqual(['default']);
+
+      supply3.off();
+      expect(active.has('default')).toBe(true);
+      expect(active.has('test')).toBe(false);
+      expect([...active]).toEqual(['default']);
+    });
+
+    describe('when', () => {
+      it('activates immediately', async () => {
+
+        const activator = jest.fn(() => new Supply());
+
+        role.when('default', activator);
+        expect(activator).toHaveBeenCalledWith(control, 'default', await role.read);
+      });
+      it('deactivates when another role added', () => {
+
+        const activationSupply = new Supply();
+        const activator = jest.fn(() => activationSupply);
+
+        role.when('default', activator);
+        role.add('test');
+        expect(activationSupply.isOff).toBe(true);
+      });
+      it('re-activates when another role removed', () => {
+
+        const activator = jest.fn(() => new Supply());
+
+        role.when('default', activator);
+        role.add('test').off();
+        expect(activator).toHaveBeenCalledTimes(2);
+      });
+    });
+  });
+});
---
+++ src/aspects/role.aspect.ts
@@ -0,0 +1,330 @@
+import { AfterEvent, AfterEvent__symbol, EventKeeper, mapAfter_, trackValue } from '@proc7ts/fun-events';
+import { noop, Supply, SupplyPeer } from '@proc7ts/primitives';
+import { InAspect, InAspect__symbol } from '../aspect';
+import { InControl } from '../control';
+
+const InRole__aspect: InAspect<InRole<any>, 'role'> = {
+
+  applyTo<TValue>(control: InControl<TValue>): InAspect.Applied<TValue, InRole<TValue>> {
+    return {
+      instance: new InControlRole<TValue>(control),
+      convertTo: noop,
+    };
+  },
+
+};
+
+/**
+ * A role of input control.
+ *
+ * Contains arbitrary named roles. A special role `'default'` considered active when no other roles activated.
+ *
+ * Roles may be used to conditionally activate other input aspects of the control.
+ *
+ * @typeParam TValue - Input value type.
+ */
+export abstract class InRole<TValue> implements EventKeeper<[InRole.Active]> {
+
+  static get [InAspect__symbol](): InAspect<InRole<any>, 'role'> {
+    return InRole__aspect;
+  }
+
+  /**
+   * An `AfterEvent` keeper of active roles.
+   *
+   * The `[AfterEvent__symbol]` method returns this value.
+   */
+  abstract readonly read: AfterEvent<[InRole.Active]>;
+
+  [AfterEvent__symbol](): AfterEvent<[InRole.Active]> {
+    return this.read;
+  }
+
+  /**
+   * Adds named role to control.
+   *
+   * The named role becomes activate. To deactivate it the returned supply has to be cut off.
+   *
+   * The same role may be added multiple times. In that case the role will be deactivated once all role supplies cut
+   * off.
+   *
+   * @param role - A name of the role to add.
+   *
+   * @returns A role supply. Removes the added role once cut off.
+   */
+  abstract add(role: string): Supply;
+
+  /**
+   * Registers activator of the given role.
+   *
+   * The given activator would be issued once the given role {@link add activated}. A supply instance returned
+   * by activator call will be cut off once the role deactivated.
+   *
+   * @param role - Target role name.
+   * @param activator - Role activator.
+   *
+   * @returns Activator supply. Removes the registered activator once cut off.
+   */
+  abstract when(role: string, activator: InRole.Activator<TValue>): Supply;
+
+}
+
+export namespace InRole {
+
+  /**
+   * Role activator signature.
+   *
+   * @typeParam TValue - Input value type.
+   */
+  export type Activator<TValue> =
+  /**
+   * @param control - A control the role is activate for.
+   * @param role - Activated role name.
+   * @param active - Active control role.
+   *
+   * @returns Activation supply peer. Its supply will be cut off once the role deactivated.
+   */
+      (
+          this: void,
+          control: InControl<TValue>,
+          role: string,
+          active: Active,
+      ) => SupplyPeer;
+
+  /**
+   * Active input control role.
+   *
+   * Contains all roles {@link InRole.add added} to control.
+   *
+   * Implements an `Iterable` interface by iterating over all active role names.
+   */
+  export interface Active extends Iterable<string> {
+
+    /**
+     * Checks whether the given role is active.
+     *
+     * @param role - Target role name.
+     *
+     * @returns `true` if the given role is {@link InRole.add added} to controller, or `false` otherwise.
+     */
+    has(role: string): boolean;
+
+  }
+
+}
+
+class InRole$Active {
+
+  static create(): InRole$Active {
+
+    const result = new InRole$Active(new Map());
+
+    result.add('default', true);
+
+    return result;
+  }
+
+  readonly active: InRole.Active;
+  private _activate: (this: void) => void = noop;
+  private _defaultSupply!: Supply;
+
+  private constructor(
+      readonly roles: Map<string, InRole$Named>,
+  ) {
+    this.active = {
+      [Symbol.iterator]() {
+        return roles.keys();
+      },
+      has(role: string) {
+        return roles.has(role);
+      },
+    };
+  }
+
+  modify(): InRole$Active {
+
+    const result = new InRole$Active(this.roles);
+
+    result._defaultSupply = this._defaultSupply;
+
+    return result;
+  }
+
+  add(role: string, isDefault: boolean): Supply | undefined {
+
+    const named = this.roles.get(role);
+
+    if (named) {
+      // Already active.
+      ++named.active;
+      return;
+    }
+
+    const supply = new Supply();
+
+    this.roles.set(
+        role,
+        {
+          active: 1,
+          supply,
+        },
+    );
+
+    if (isDefault) {
+      this._defaultSupply = supply.whenOff(() => {
+
+        const toRemove = this.roles.get(role)!;
+
+        if (!--toRemove.active) {
+          this.roles.delete(role);
+        }
+      });
+    } else {
+
+      const defaultSupply = this._defaultSupply;
+
+      this.activateBy(() => defaultSupply.off());
+    }
+
+    return supply;
+  }
+
+  remove(role: string, reason: unknown): void {
+
+    const named = this.roles.get(role)!;
+
+    if (--named.active) {
+      // Still active.
+      return;
+    }
+
+    this.roles.delete(role);
+    this.activateBy(() => named.supply.off(reason));
+  }
+
+  activateBy(activator: () => void): void {
+
+    const prevActivator = this._activate;
+
+    this._activate = () => {
+      prevActivator();
+      activator();
+    };
+  }
+
+  activate(): void {
+
+    const activator = this._activate;
+
+    this._activate = noop;
+
+    activator();
+  }
+
+}
+
+interface InRole$Named {
+  active: number;
+  readonly supply: Supply;
+}
+
+class InControlRole<TValue> extends InRole<TValue> {
+
+  private readonly _active = trackValue<InRole$Active>(InRole$Active.create());
+  private readonly _activators = new Map<string, Map<Supply, InRole.Activator<TValue>>>();
+  readonly read: AfterEvent<[InRole.Active]>;
+
+  constructor(private readonly _control: InControl<TValue>) {
+    super();
+    this._active.on(active => active.activate());
+    this.read = this._active.read.do(mapAfter_(({ active }) => active));
+  }
+
+  add(role: string): Supply {
+
+    const active = this._active.it.modify();
+
+    this._add(active, role);
+    this._active.it = active;
+
+    return new Supply(reason => {
+
+      const active = this._active.it.modify();
+
+      active.remove(role, reason);
+      if (!active.roles.size) {
+        // No more active roles left.
+        // Enable `default` role.
+        this._add(active, 'default', true);
+      }
+
+      this._active.it = active;
+    });
+  }
+
+  when(role: string, activator: InRole.Activator<TValue>): Supply {
+
+    let activators = this._activators.get(role);
+
+    if (!activators) {
+      activators = new Map();
+      this._activators.set(role, activators);
+    }
+
+    const supply = new Supply();
+
+    activators.set(supply, activator);
+    supply.whenOff(() => {
+      activators!.delete(supply);
+      if (!activators!.size) {
+        this._activators.delete(role);
+      }
+    });
+
+    const named = this._active.it.roles.get(role);
+
+    if (named) {
+      // The role already active.
+      // Issue activator immediately.
+      named.supply.cuts(activator(this._control, role, this._active.it.active));
+    }
+
+    return supply;
+  }
+
+  private _add(active: InRole$Active, role: string, isDefault = false): void {
+
+    const activatedSupply = active.add(role, isDefault);
+
+    if (activatedSupply) {
+      // Role activated
+
+      const activators = this._activators.get(role);
+
+      if (activators) {
+        // Issue activators
+        for (const activator of activators.values()) {
+          activatedSupply.cuts(activator(this._control, role, this._active.it.active));
+        }
+      }
+    }
+  }
+
+}
+
+declare module '../aspect' {
+
+  export namespace InAspect.Application {
+
+    export interface Map<TInstance, TValue> {
+
+      /**
+       * Input role application type.
+       */
+      role(): InRole<TValue>;
+
+    }
+
+  }
+
+}
